2b61e7936559c5bd4cc1059c5970c8048c208bce
felixthe8 <felixthe8@gmail.com>
2016-01-03 10:13:04 -0800

    Create Deck.java

---
+++ BlackJack-Java/Deck.java
@@ -0,0 +1,82 @@
+package Cards;
+
+import java.util.*;
+public class Deck 
+{
+	private static ArrayList <Card> cards = new ArrayList <Card>();
+	private static ArrayList <Integer> Existing_Index = new ArrayList <Integer> ();
+	private  int index;
+	private Random gen = new Random ();
+	private int count;
+	//constructor the the Deck
+	public Deck ()
+	{
+		count = 0;
+		for (int k = 0 ; k < 4 ; k++) // keeps track of the suits
+		{
+			for (int i = 1; i<14;i++ ) //assigns the value and suit of the card
+			{
+				switch (k)
+				{
+				case 0 : cards.add( new Card (i , "Spades")); break;
+				case 1 : cards.add( new Card (i , "Clubs")); break;
+				case 2 : cards.add( new Card (i , "Diamonds")); break; 
+				case 3 : cards.add( new Card (i , "Hearts")); break ;  
+				}
+				
+			}
+		}
+		count = 51;
+	}
+	
+	//draws a card from a random spot in the deck
+	public Card drawFromDeck()
+	{
+		 index = gen.nextInt(cards.size());
+		return cards.get(index);
+	}
+	
+	//prints the size of the deck of cards
+	public static int getTotalCards ()
+	{
+		return cards.size();
+	}
+	
+	public  void print()
+	{
+		for (int i = 0 ; i < cards.size(); i ++)
+		{
+			System.out.print( cards.get(i) + "\n");
+		}
+	}
+	
+	public void shuffleDeck()
+	{
+		for (int a = 0 ; a <=10; a++)
+		{
+			Collections.shuffle(cards);
+		}
+	}
+	
+	public ArrayList <Card> getDeck ()
+	{
+		return Deck.cards;
+	}
+	
+	public Card topCard () //returns the top card of the deck without decrementing count
+	{
+		return cards.get(count);
+	}
+	
+	public Card dealCard ()
+	{
+		Card temp = cards.get(count -- );
+		cards.remove(cards.indexOf(temp));
+		return temp;
+	}
+	
+	public int getCount () //returns the number of cards in the deck
+	{
+		return (count + 1);
+	}
+}
